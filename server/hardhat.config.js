1c4259b286571a2865e47e6a47548c53410b1479
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-09-20 02:37:41 +0530

    Added react

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    volta: {
+      url: 'https://volta-rpc.energyweb.org/',
+      accounts: process.env.ACCOUNT_PRIVATE
+    }
+  }
 };
